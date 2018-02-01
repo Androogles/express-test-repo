703a72750171455c708c0f676661bec2ef001eb5
Androogles <35560498+Androogles@users.noreply.github.com>
2018-02-01 12:56:54 +0100

    færdiggjort opgave 2

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const logger = require('morgan');
+app.use(logger('dev'));
+
+
+// Opgave 1
 app.get('/api/test', (req, res) => {
     res.json({
         text: "Hello World"
@@ -15,6 +20,25 @@ app.get('/api/opgave', (req, res) => {
         By: "Albertslund"
     });
 });
+
+// Opgave 2
+app.get('/api/route1', (req, res) => {
+    res.json({
+        Farve1: "Rød",
+        Farve2: "Grøn",
+        Farve3: "Blå"
+    });
+});
+
+app.get('/api/route2', (req, res) => {
+    res.json({
+        Tal1: "5",
+        Tal2: "10",
+        Tal3: "15",
+        Tal4: "20"
+    });
+});
+
 
 app.use(express.static('public'));
 
